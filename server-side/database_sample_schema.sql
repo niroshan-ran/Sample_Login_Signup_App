17f2f314d917a694345b981d9e3b287ef6f69a25
niroshan <inranasinghe2208@gmail.com>
2021-10-20 09:11:58 +0530

    Security Vulnerabilities Fixed...

--- server-side/database_sample_schema.sql
+++ server-side/database_sample_schema.sql
@@ -1,17 +1,11 @@
-# This is the Database Generation MySQL file
-
-create database sample_schema;
-
-use sample_schema;
-
 create table server_key_table
 (
-    `public_key_1`      varchar(1024) null,
-    `private_key_1`     varchar(1024) null,
-    `public_key_2`      varchar(1024) null,
-    `private_key_2`     varchar(1024) null,
-    `client_public_key` varchar(1024) null,
-    `row_id`            int auto_increment,
+    public_key_1      text null,
+    private_key_1     text null,
+    public_key_2      text null,
+    private_key_2     text null,
+    client_public_key text null,
+    row_id            int auto_increment,
     constraint server_key_table_pk
         unique (row_id)
 );
@@ -21,6 +15,10 @@ alter table server_key_table
 
 create table user_table
 (
-    `email`    varchar(100) not null primary key,
-    `password` varchar(100) not null
+    email       varchar(100) not null
+        primary key,
+    password    text         not null,
+    firstname   text         null,
+    lastname    text         null,
+    generalUser tinyint(1)   not null
 );
